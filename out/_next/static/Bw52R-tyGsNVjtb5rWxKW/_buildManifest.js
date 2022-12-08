b99ce9e9a866a8761a0b013d7ca3b8d18f47f3ee
Gil Sousa <tiago.goncalves@deliveryhero.com>
2022-12-08 12:25:35 +0100

    First website build

---
+++ out/_next/static/Bw52R-tyGsNVjtb5rWxKW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0dd76035fdfc4c62.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/initiatives":["static/chunks/pages/initiatives-faf4eed2e8441a4d.js"],sortedPages:["/","/_app","/_error","/initiatives"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

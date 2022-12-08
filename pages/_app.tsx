9d0e85af6894b2da67f8eaf06e698b9fac66490a
Gil Sousa <tiagosousafl@gmail.com>
2022-12-08 11:41:45 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,79 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle");
+  }, []);
+
+  useEffect(() => {
+    import("jquery/dist/jquery");
+  }, []);
+
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap.bundle")
+      : null;
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            RPL Guild
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Initiatives
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className="py-4">
+          <Component {...pageProps} />
+        </div>
+      </div>
+
+      <footer className="bg-light text-center text-lg-start">
+        <div className="text-center p-3">
+          Get in touch with us on Slack in{" "}
+          <a
+            className="text-dark"
+            href="https://deliveryhero.slack.com/archives/C0402517ESV"
+          >
+            #pd-rs-platform_guild
+          </a>
+        </div>
+      </footer>
+    </>
+  );
 }

fdcb609978ec35d14924e885e7bc54fb804191d2
Gil Sousa <tiago.goncalves@deliveryhero.com>
2022-12-09 10:40:17 +0100

    Remove build folders

--- out/_next/static/Bw52R-tyGsNVjtb5rWxKW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0dd76035fdfc4c62.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/initiatives":["static/chunks/pages/initiatives-faf4eed2e8441a4d.js"],sortedPages:["/","/_app","/_error","/initiatives"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
